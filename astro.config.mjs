3372eadf62d561304a7319db55944f27dbbbf809
danihe <danihe@stud.ntnu.no>
2025-10-10 02:56:02 +0200

    added astro icon integration to config

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,7 +1,9 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
+import icon from 'astro-icon';
 
 // https://astro.build/config
 export default defineConfig({
     base: '/portfolio', 
+    integrations: [icon()], 
 });
